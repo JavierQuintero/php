ca9e52bd390a8451cb77c85de4786b9198d7b005
Javier <ejvquin@gmail.com>
2019-06-22 21:39:51 -0500

    Modelo Vista Controlador//
    Se inicia la estructura basica de MVC sin conecciones.

---
+++ MVC/views/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+</head>
+<body>
+    <header>
+        <h1>LOGOTIPO</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Servicios</a></li>
+            <li><a href="">Contactos</a></li>
+        </ul>
+    </nav>
+    <section>
+        <h1>PAGINA DE INICIO</h1>
+    </section>
+</body>
+</html>
