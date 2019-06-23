0e2fe97b8b21a13edf084cc1e1b03508ab9eb676
Javier <ejvquin@gmail.com>
2019-06-23 15:57:16 -0500

    Nuevo estilo y modularidad del menu
    Se organiza mediante a sass un menu lateral con efectos en css y
    mediante conectividad _GET vinculamos las paginas

--- MVC/views/template.php
+++ MVC/views/template.php
@@ -4,23 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="../css/estilos.css" rel="stylesheet"/>
+    <link href="../sass/estilos.css" rel="stylesheet"/>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet"> -->
     <title>Template</title>
 </head>
 <body>
     <header>
         <h1>LOGOTIPO</h1>
     </header>
-    <nav>
-        <ul>
-            <li><a href="">NOSOTROS</a></li>
-            <li><a href="">LA CARTA</a></li>
-            <li><a href="">LA BARRA</a></li>
-            <li><a href="">RESERVAS</a></li>
-            <li><a href="">BLOG</a></li>
-            <li><a href="">CONCTACTO</a></li>
-        </ul>
-    </nav>
+        <?php
+
+            include "modules/elem/navegation.php"
+        
+        ?>
     <section>
         <h1>PAGINA DE INICIO</h1>
     </section>
