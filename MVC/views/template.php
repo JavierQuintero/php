0ef2d4408df7c3a728ba83ec6684828a58372df3
Javier <ejvquin@gmail.com>
2019-06-23 09:13:39 -0500

    Se aniaden estilos a la pagina
    Se modulariza el estilo de la pagina mediante css y sass
    Automaticamente el copilador de codigo crea un documento de
    configuracion para copilacion de css.
    Copilador "prepros".

--- MVC/views/template.php
+++ MVC/views/template.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../css/estilos.css" rel="stylesheet"/>
     <title>Template</title>
 </head>
 <body>
@@ -12,10 +13,12 @@
     </header>
     <nav>
         <ul>
-            <li><a href="">Inicio</a></li>
-            <li><a href="">Nosotros</a></li>
-            <li><a href="">Servicios</a></li>
-            <li><a href="">Contactos</a></li>
+            <li><a href="">NOSOTROS</a></li>
+            <li><a href="">LA CARTA</a></li>
+            <li><a href="">LA BARRA</a></li>
+            <li><a href="">RESERVAS</a></li>
+            <li><a href="">BLOG</a></li>
+            <li><a href="">CONCTACTO</a></li>
         </ul>
     </nav>
     <section>
